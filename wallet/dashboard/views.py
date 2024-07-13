e23e364847b60e66347b9e77d299b647619179d8
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-07-13 11:28:48 -0300

    Separate pages from the base html

--- wallet/dashboard/views.py
+++ wallet/dashboard/views.py
@@ -1,40 +1,38 @@
 from django.shortcuts import render
 from dashboard.forms import AddWalletForm
 from dashboard import models
-# Create your views here.
+from django.http import HttpResponseRedirect
 
 def add_wallet(request):
     form = None
-    if request.GET.get('form'):
-        form = request.GET.get('form')
-
-    else:
+    if request.method == 'GET':
         form = AddWalletForm()
 
-    if request.method == 'POST':
-        label = request.POST.get('wallet_label')
-        amount = request.POST.get('wallet_amount')
+    elif request.method == 'POST':
+        label = request.POST.get('label')
+        amount = request.POST.get('amount')
 
         form = AddWalletForm({'label': label, 'amount': amount})
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect('')
 
     context = {
         'form': form,
         'title': 'Dashboard'
     }
-    return render(request, 'templates/base.html', context)
+    return render(request, 'templates/table_pages.html', context)
 
 def list_wallets(request):
     all_wallets = models.Wallet.objects.all()
     context = {
         'wallets': all_wallets
     }
-    return render(request, 'templates/base.html', context)
+    return render(request, 'templates/table_pages.html', context)
 
 def list_transactions(request):
     all_transactions = models.Transaction.objects.all()
     context = {
         'transactions': all_transactions
     }
-    return render(request, 'templates/base.html', context)
+    return render(request, 'templates/table_pages.html', context)
